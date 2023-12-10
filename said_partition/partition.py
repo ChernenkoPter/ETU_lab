2022480b967366e8e52874818c0ac235cfa5c6ba
Chernenko Petr <lab.chern@mail.ru>
2023-12-11 02:31:13 +0300

    Написал функцию вычисления средней длины слова для разбиения алфавита

--- said_partition/partition.py
+++ said_partition/partition.py
@@ -1,8 +1,21 @@
-from entropy import entropy_2
+import huffman as huf
+from math import log
 
 
 def partition_entropy(partition, words_to_counts):
     result = 0
     distribution = list(x[1] for x in words_to_counts)
-    # TODO реализовать вычисление средней длины кодового слова по размеру групп и количеству символов в алфавите
+
+    # get distribution of groups
+    group_distribution = dict()
+    for i in range(len(partition)):
+        k = sum(partition[0:i])
+        group_distribution[i] = sum(distribution[k:partition[i] + 1])
+
+    group_nodes = group_distribution
+    group_coding_prefixes = huf.get_huffman_coding(group_nodes)
+
+    result = 0  # (длина префикса + длина символов в группе) * количество символов в группе
+    for i in range(len(group_coding_prefixes)):
+        result += (len(group_coding_prefixes[i]) + log(partition[i], 2)) * partition[i]
     return result
