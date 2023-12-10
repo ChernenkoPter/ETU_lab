3b54929241babba8bebd8ebe541e7f988cc32dd4
Chernenko Petr <lab.chern@mail.ru>
2023-12-11 02:44:53 +0300

    Исправление опечаток

--- said_partition/main.py
+++ said_partition/main.py
@@ -5,9 +5,9 @@ from diadic_partition import find_optimal_diadic_partition_group_sizes
 
 def optimal_partition(words_to_counts, is_linear):
     if is_linear:  # find groups for optimal partitions
-        group_sizes = find_optimal_linear_partition_group_sizes(words_to_counts, [1, len(words_count)])
+        group_sizes = find_optimal_linear_partition_group_sizes(words_to_counts, [1, len(words_to_counts)])
     else:
-        group_sizes = find_optimal_diadic_partition_group_sizes(words_to_counts, [1, len(words_count)])
+        group_sizes = find_optimal_diadic_partition_group_sizes(words_to_counts, [1, len(words_to_counts)])
 
     it = iter(words_to_counts)
     return [[next(it) for _ in range(size)] for size in group_sizes]  # break words_to_counts into these groups
